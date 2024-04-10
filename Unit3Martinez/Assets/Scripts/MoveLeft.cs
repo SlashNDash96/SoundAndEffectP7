22fd25a56c524d5adfdd5a5398b71caa59e84553
Jayden Martinez <724583@student.dusd.net>
2024-04-10 15:29:32 -0700

    NEW ASSIGNMENT
    Jumpin' over traffic cones in a city for some reason

---
+++ Unit3Martinez/Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private float speed = 30;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.left * Time.deltaTime * speed);
+    }
+}
